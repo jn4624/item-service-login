178bd2ac0d15e7e9c95ecfb4320803f666ff2367
LimJiNa <jn4624@gmail.com>
2024-03-19 00:11:47 +0900

    회원 가입 기능 구현

--- src/main/java/hello/item/TestDataInit.java
+++ src/main/java/hello/item/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.item;
 
 import hello.item.domain.item.Item;
 import hello.item.domain.item.ItemRepository;
+import hello.item.domain.member.Member;
+import hello.item.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TestDataInit {
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -18,5 +21,11 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("admin");
+        member.setPassword("admin!");
+        member.setName("관리자");
+        memberRepository.save(member);
     }
 }
