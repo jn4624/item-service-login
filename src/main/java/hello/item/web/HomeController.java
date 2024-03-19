b4f123eef7d9ae87feb5f73ef58b07ac4f929132
LimJiNa <jn4624@gmail.com>
2024-03-19 21:36:46 +0900

    로그인 처리 - Cookie 사용

--- src/main/java/hello/item/web/HomeController.java
+++ src/main/java/hello/item/web/HomeController.java
@@ -1,12 +1,38 @@
 package hello.item.web;
 
+import hello.item.domain.member.Member;
+import hello.item.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
     }
 }
--- src/main/java/hello/item/web/login/LoginController.java
+++ src/main/java/hello/item/web/login/LoginController.java
@@ -2,6 +2,8 @@ package hello.item.web.login;
 
 import hello.item.domain.login.LoginService;
 import hello.item.domain.member.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -23,7 +25,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -37,8 +39,26 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        /**
+         * 로그인 성공 처리
+         * 쿠키에 만료 날짜를 생략하면 세션 쿠키(브라우저 종료시 모두 종료)
+         */
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
