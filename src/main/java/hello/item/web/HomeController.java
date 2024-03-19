831362634523c220d7acdef436ab309c34738cac
LimJiNa <jn4624@gmail.com>
2024-03-19 23:16:17 +0900

    로그인 처리 - 서블릿 Http 세션2

--- src/main/java/hello/item/web/HomeController.java
+++ src/main/java/hello/item/web/HomeController.java
@@ -2,12 +2,11 @@ package hello.item.web;
 
 import hello.item.domain.member.Member;
 import hello.item.web.session.SessionManager;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
@@ -52,17 +51,28 @@ public class HomeController {
 //        return "loginHome";
 //    }
 
+//    @GetMapping("/")
+//    public String homeLoginV3(HttpServletRequest request, Model model) {
+//        // 세션 관리자에 저장된 회원 정보 조회
+//        HttpSession session = request.getSession(false);
+//
+//        if (session == null) {
+//            return "home";
+//        }
+//
+//        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+//
+//        if (member == null) {
+//            return "home";
+//        }
+//
+//        model.addAttribute("member", member);
+//
+//        return "loginHome";
+//    }
+
     @GetMapping("/")
-    public String homeLoginV3(HttpServletRequest request, Model model) {
-        // 세션 관리자에 저장된 회원 정보 조회
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            return "home";
-        }
-
-        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
-
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
         if (member == null) {
             return "home";
         }
