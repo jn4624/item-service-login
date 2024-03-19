f2b5cd01f67a4e7a3b61412b2b5fa0312fd55136
LimJiNa <jn4624@gmail.com>
2024-03-19 22:57:58 +0900

    로그인 처리 - 직접 구현한 Session 서비스에 적용

--- src/main/java/hello/item/web/HomeController.java
+++ src/main/java/hello/item/web/HomeController.java
@@ -1,37 +1,52 @@
 package hello.item.web;
 
 import hello.item.domain.member.Member;
-import hello.item.domain.member.MemberRepository;
+import hello.item.web.session.SessionManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
 //    public String home() {
 //        return "home";
 //    }
 
+//    @GetMapping("/")
+//    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+//        if (memberId == null) {
+//            return "home";
+//        }
+//
+//        // 로그인
+//        Member loginMember = memberRepository.findById(memberId);
+//
+//        if (loginMember == null) {
+//            return "home";
+//        }
+//
+//        model.addAttribute("member", loginMember);
+//
+//        return "loginHome";
+//    }
+
     @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
-        if (memberId == null) {
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        if (member == null) {
             return "home";
         }
 
-        // 로그인
-        Member loginMember = memberRepository.findById(memberId);
-
-        if (loginMember == null) {
-            return "home";
-        }
-
-        model.addAttribute("member", loginMember);
+        model.addAttribute("member", member);
 
         return "loginHome";
     }
--- src/main/java/hello/item/web/login/LoginController.java
+++ src/main/java/hello/item/web/login/LoginController.java
@@ -2,7 +2,9 @@ package hello.item.web.login;
 
 import hello.item.domain.login.LoginService;
 import hello.item.domain.member.Member;
+import hello.item.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +20,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class LoginController {
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm) {
         return "login/loginForm";
     }
 
+//    @PostMapping("/login")
+//    public String login(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
+//        if (bindingResult.hasErrors()) {
+//            return "login/loginForm";
+//        }
+//
+//        Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+//
+//        log.info("login? = {}", loginMember);
+//
+//        if (loginMember == null) {
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+//            return "login/loginForm";
+//        }
+//
+//        /**
+//         * 로그인 성공 처리
+//         * 쿠키에 만료 날짜를 생략하면 세션 쿠키(브라우저 종료시 모두 종료)
+//         */
+//        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+//
+//        response.addCookie(idCookie);
+//
+//        return "redirect:/";
+//    }
+
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
+    public String loginV2(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -41,18 +70,22 @@ public class LoginController {
 
         /**
          * 로그인 성공 처리
-         * 쿠키에 만료 날짜를 생략하면 세션 쿠키(브라우저 종료시 모두 종료)
+         * 세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관(브라우저 종료시 모두 종료)
          */
-        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
-
-        response.addCookie(idCookie);
+        sessionManager.createSession(loginMember, response);
 
         return "redirect:/";
     }
 
+//    @PostMapping("/logout")
+//    public String logout(HttpServletResponse response) {
+//        expireCookie(response, "memberId");
+//        return "redirect:/";
+//    }
+
     @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
         return "redirect:/";
     }
 
