ce79902d65af931ea0a25238b90024dd401d4500
LimJiNa <jn4624@gmail.com>
2024-03-21 22:15:53 +0900

    스프링 인터셉터 - 요청 로그

--- src/main/java/hello/item/WebConfig.java
+++ src/main/java/hello/item/WebConfig.java
@@ -2,21 +2,24 @@ package hello.item;
 
 import hello.item.web.filter.LogFilter;
 import hello.item.web.filter.LoginCheckFilter;
+import hello.item.web.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new LogFilter());
-        filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("/*");
-        return filterFilterRegistrationBean;
-    }
+public class WebConfig implements WebMvcConfigurer {
+//    @Bean
+//    public FilterRegistrationBean logFilter() {
+//        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+//        filterFilterRegistrationBean.setFilter(new LogFilter());
+//        filterFilterRegistrationBean.setOrder(1);
+//        filterFilterRegistrationBean.addUrlPatterns("/*");
+//        return filterFilterRegistrationBean;
+//    }
 
     @Bean
     public FilterRegistrationBean loginCheckFilter() {
@@ -25,5 +28,13 @@ public class WebConfig {
         filterFilterRegistrationBean.setOrder(2);
         filterFilterRegistrationBean.addUrlPatterns("/*");
         return filterFilterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
     }
 }
